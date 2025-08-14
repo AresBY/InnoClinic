0ce3c807a1a52d969be6ddf610f9f178ee0f4db5
Игорь Грищук <igor_gr@holdingbp.ru>
2025-08-14 12:54:38 +0300

    US-51 View patient’s profile by admin

---
+++ Profiles/Application/Features/Patient/Queries/GetPatientProfileByReceptionist/GetPatientProfileByReceptionistQuery.cs
@@ -0,0 +1,11 @@
+﻿using InnoClinic.Profiles.Application.DTOs;
+
+using MediatR;
+
+namespace InnoClinic.Profiles.Application.Features.Patient.Queries.GetPatientProfileByReceptionist
+{
+    public sealed class GetPatientProfileByReceptionistQuery : IRequest<PatientProfileDto?>
+    {
+        public Guid PatientId { get; set; }
+    }
+}
