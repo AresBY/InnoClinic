d4245f6ebdd26f795c672f2b8f4eeabe4da72af8
Игорь Грищук <igor_gr@holdingbp.ru>
2025-08-14 14:21:51 +0300

    US-57 View receptionists

---
+++ Profiles/Application/Features/Receptionist/Queries/GetAllReceptionists/GetAllReceptionistsQuery.cs
@@ -0,0 +1,11 @@
+﻿using InnoClinic.Profiles.Application.DTOs;
+
+using MediatR;
+
+
+namespace InnoClinic.Profiles.Application.Features.Receptionist.Queries
+{
+    public sealed class GetAllReceptionistsQuery : IRequest<List<ReceptionistProfileDto>>
+    {
+    }
+}
---
+++ Profiles/Application/Features/Receptionist/Queries/GetAllReceptionists/GetAllReceptionistsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using InnoClinic.Profiles.Application.DTOs;
+using InnoClinic.Profiles.Application.Interfaces.Repositories;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoClinic.Profiles.Application.Features.Receptionist.Queries.GetAllReceptionists
+{
+    public sealed class GetAllReceptionistsQueryHandler : IRequestHandler<GetAllReceptionistsQuery, List<ReceptionistProfileDto>>
+    {
+        private readonly IReceptionistProfileRepository _repository;
+
+        public GetAllReceptionistsQueryHandler(IReceptionistProfileRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<ReceptionistProfileDto>> Handle(GetAllReceptionistsQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.Query()
+                .Select(r => new ReceptionistProfileDto
+                {
+                    Id = r.Id,
+                    FirstName = r.FirstName,
+                    LastName = r.LastName,
+                    MiddleName = r.MiddleName
+                })
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
