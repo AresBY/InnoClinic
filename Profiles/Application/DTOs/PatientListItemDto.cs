0fc7d840bf272ed2973ff366dfe0d4929fe4a328
Игорь Грищук <igor_gr@holdingbp.ru>
2025-08-14 12:34:23 +0300

    US-50 Search patient by name by admin

---
+++ Profiles/Application/DTOs/PatientListItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace InnoClinic.Profiles.Application.DTOs
+{
+    public sealed record PatientListItemDto : BaseProfileDto
+    {
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; } = default!;
+
+        public string? FullName { get; set; }
+    }
+}
---
+++ Profiles/Application/Features/Patient/Queries/SearchPatients/SearchPatientsQuery.cs
@@ -0,0 +1,11 @@
+﻿using InnoClinic.Profiles.Application.DTOs;
+
+using MediatR;
+
+namespace InnoClinic.Profiles.Application.Features.Patient.Queries.SearchPatients
+{
+    public sealed class SearchPatientsQuery : IRequest<List<PatientListItemDto>>
+    {
+        public string? FullName { get; set; }
+    }
+}
